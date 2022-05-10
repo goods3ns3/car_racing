bb66feb132f0f8c56421e44d9ed8862e6b25efc5
Kryzhaniuk Oleksandr <kryzhaniuko@gmail.com>
2022-05-10 17:43:21 +0300

    base output

--- carracing/journey/admin.py
+++ carracing/journey/admin.py
@@ -5,10 +5,10 @@ from .models import Car, Journey
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'car_number']
+    list_display = ['id', 'name', 'car_number', 'speed']
     prepopulated_fields = {'car_number': ('name',)}
 
 
 @admin.register(Journey)
 class JourneyAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'car', 'time_start', 'time_finish']
+    list_display = ['id', 'user_name', 'car', 'time_start', 'time_finish']
--- carracing/journey/views.py
+++ carracing/journey/views.py
@@ -1,13 +1,48 @@
-from django.views.generic import FormView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView, ListView
+
+from .forms import UserJourneyForm
+from .models import Journey, Car
 
 
-class JourneyView(TemplateView):
+class JourneyView(FormView):
+    model = Journey
     template_name = 'journey/journey_form.html'
+    form_class = UserJourneyForm
+
+    def form_valid(self, form):
+        self.time_in_trip = form.save_journey(self.request)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        success_url = reverse_lazy(
+            'journey_success',
+            kwargs={'time_in_trip': self.time_in_trip}
+        )
+        return success_url
 
 
-class VehicleStatsView(TemplateView):
+class JourneySuccessView(TemplateView):
+    template_name = 'journey/journey_success.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        time_in_trip = kwargs.get('time_in_trip')
+        ctx['time_in_trip'] = time_in_trip
+        return ctx
+
+
+class VehicleStatsView(ListView):
     template_name = 'journey/vehicle_stats.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        return Car.objects.all()
 
 
-class PassengerStatsView(TemplateView):
+class PassengerStatsView(ListView):
     template_name = 'journey/passengers_stats.html'
+    context_object_name = 'passengers'
+
+    def get_queryset(self):
+        return Journey.objects.all()
